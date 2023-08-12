d7c3854929c54b9554e5dc88cd1ef93daa72ad83
aayushma001 <katuwalaayushma100@gmail.com>
2023-08-12 22:36:45 +0545

    all-any

---
+++ day-13/4_dict_operations_fxns.py
@@ -0,0 +1,12 @@
+student = {"name": "jon", "age": 25, "department": "CS"}
+print("name" in student) #True
+print("jon" in student) # False
+
+
+# Built-in fucntions
+print(len(student)) # 3
+
+result = sorted(student)
+print(result) # [age", "department", "name"]
+
+print(str(student)) # "{"name": "jon", "age": 25, "department": "CS"}"
