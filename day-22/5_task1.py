07b9427f23cd2e7d000e13c27713e2b61084ad1e
aayushma001 <katuwalaayushma100@gmail.com>
2023-08-22 09:18:41 +0600

    virtual environment

--- day-22/5_task1.py
+++ day-22/5_task1.py
@@ -2,3 +2,21 @@
 # Add the radius of the circles and print the result. Do the above task using a method and
 # then a magic method.Compare the size of the circle and print the result using magic method.
 class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def add_radius(self, obj2):
+        return obj2.radius+self.radius
+    def __add__(self, other):
+        return self.other + other.radius
+
+
+c1 = Circle(3)
+c2 = Circle(5)
+result = c1.radius + c2.radius
+print(result) # 8
+
+result = c1.add_radius(c2)
+print(result) # 8
+
+result = c1+ c2
+print(result) # 8
